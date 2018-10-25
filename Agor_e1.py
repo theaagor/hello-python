38e16da873239413d15fee00b747f0fa2b3619a9
theaagor <mariatheresaagor16@gmail.com>
2018-10-25 22:51:52 +0800

    Agor e1 commit

---
+++ Agor_e1.py
@@ -0,0 +1,12 @@
+data ={ "Name":" Maria Theresa Agor",
+"Spirit Animal":"Dog",
+"Reason":"I am a carefree person",
+"Hobby":"Reading ebooks and watching movies,animes,or kdramas",
+"Profession":"Musician"
+}
+
+print("I am {}.".format(data["Name"]))
+print("My spirit animal is{},".format(data["Spirit Animal"]))
+print("because {}.".format(data["Reason"]))
+print("When not in school, I love to {}.".format(data["Hobby"]))
+print("I dream of being a/an {} in the future.".format(data["Profession"]))
--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 print("Hello, World")
 print ("My name is {}. I am {} y/o." \
 .format("Agor", 17))
+
+print("ADNU",2018, sep="", end="")
+print("CSNHS")
